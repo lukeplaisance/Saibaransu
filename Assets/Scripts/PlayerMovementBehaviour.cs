7b49e504f9be2f3df88231a28ab42de53c383a4e
lukeplaisance17 <31013260+lukeplaisance17@users.noreply.github.com>
2018-06-19 17:16:15 -0500

    wokring on PlayerMovement script

---
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+	}
+}
