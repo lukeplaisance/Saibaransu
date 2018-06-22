7c4852269e4489c4ac128bf38bac41e109fe302a
lukeplaisance17 <31013260+lukeplaisance17@users.noreply.github.com>
2018-06-22 15:33:23 -0500

    started working on the the cameras

---
+++ Assets/Scripts/PlayerCameraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraBehaviour : MonoBehaviour
+{
+    float fov = Camera.main.fieldOfView;
+    float start = 20.0f;
+    float end = 10.0f;
+    public float totalTime = 1;
+    public float currentTime = 0;
+
+	void Update ()
+    {
+        var interprolant = currentTime / totalTime; //get percentage. The percentage of how long it would take
+        if (Input.GetButton("Fire2"))
+        {
+            currentTime = currentTime + Time.deltaTime;
+            if (currentTime >= totalTime)
+            {
+                currentTime = 0;
+            }
+            fov = Mathf.Lerp(start, end, interprolant);
+            Camera.main.fieldOfView = fov;
+        }
+        else
+            return;
+	}
+}
--- Assets/Scripts/PlayerMovementBehaviour.cs
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -11,13 +11,19 @@ public class PlayerMovementBehaviour : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
     {
-        var moveHorizontal = Input.GetAxis("Horizontal");
-        var moveVertical = Input.GetAxis("Vertical");
-        var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
+        if(gameObject.CompareTag("Player1"))
+        {
+            var movement = transform.position * Time.deltaTime;
+            rb.AddForce(movement * speed);
+        }
+        if(gameObject.CompareTag("Player2"))
+        {
+            var movement = -transform.position * Time.deltaTime;
+            rb.AddForce(movement * speed);
+        }
 	}
 }
