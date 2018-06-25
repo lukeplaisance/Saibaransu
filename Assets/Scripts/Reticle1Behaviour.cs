c39911db0395608d4782fd7e49bd66b05bcf12ed
bstelly <bstelly1999@gmail.com>
2018-06-25 17:54:11 -0500

    Worked on Reticle

---
+++ Assets/Scripts/Reticle1Behaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle1Behaviour : MonoBehaviour
+{
+    private GameObject UI;
+    private RectTransform objectRectTransform;
+
+	void Start ()
+	{
+        UI = GameObject.Find("Canvas");
+	    objectRectTransform = UI.GetComponent<RectTransform>();
+	}
+
+
+
+    //USE OBJECT RECT TRANSFORM INSTEAD OF NUMBERS
+
+    void Update () {
+        if (Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") > 0)
+        {
+            if (transform.localPosition.x + 25 <= 129.5)
+            {
+                transform.position += Vector3.right * 3;
+            }
+        }
+
+        if (Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") < 0)
+        {
+            if (transform.localPosition.x - 25 >= -259)
+            {
+                transform.position += Vector3.left * 3;
+            }
+        }
+
+        if (Input.GetButton("Vertical") && Input.GetAxis("Vertical") < 0)
+        {
+            if (transform.localPosition.y - 25 >= -146)
+            {
+                transform.position += Vector3.down * 3;
+            }
+        }
+
+        if (Input.GetButton("Vertical") && Input.GetAxis("Vertical") > 0)
+        {
+            if (transform.localPosition.y + 25 <= 146)
+            {
+                transform.position += Vector3.up * 3;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Reticle2Behaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle2Behaviour : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if (Input.GetButton("Horizontal1") && Input.GetAxis("Horizontal1") > 0)
+        {
+            transform.position += Vector3.right * 3;
+        }
+        if (Input.GetButton("Horizontal1") && Input.GetAxis("Horizontal1") < 0)
+        {
+            transform.position += Vector3.left * 3;
+        }
+        if (Input.GetButton("Vertical1") && Input.GetAxis("Vertical1") < 0)
+        {
+            transform.position += Vector3.down * 3;
+        }
+        if (Input.GetButton("Vertical1") && Input.GetAxis("Vertical1") > 0)
+        {
+            transform.position += Vector3.up * 3;
+        }
+    }
+}
