0bc6ac14dc3eaddef6de7a4d45a00a9c6c8c0bd3
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-07-03 17:03:05 -0500

    added timer manager script
    Time manager script is used to slow down time so you have time to aim

---
+++ Assets/Scripts/CollisionAreaBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAreaBehaviour : MonoBehaviour
+{
+    TimeManagerBehaviour timeManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            timeManager.SlowDown();
+        }
+    }
+
+}
