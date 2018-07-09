5b2d0c9916347290783fa6733399d3075f9105ae
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-07-09 11:11:09 -0500

    changed the amount and length of slow motion

--- Assets/Scripts/CameraControllerBehaviour.cs
+++ Assets/Scripts/CameraControllerBehaviour.cs
@@ -1,32 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraControllerBehaviour : MonoBehaviour
 {
     public GameObject player;
     public bool isFirstPerson;
     public GameObject camera;
-    CameraControllerBehaviour cc;
-
-    // Use this for initialization
-    void Start ()
-    {
-        cc = GetComponent<CameraControllerBehaviour>();
-	}
 
     private void Update()
     {
         camera.transform.parent = player.transform;
-        if (Input.GetButton("Fire2"))
+        if (Input.GetButtonDown("Fire2"))
         {
             isFirstPerson = true;
             camera.transform.position = Vector3.Lerp(camera.transform.position, player.transform.position, Time.deltaTime * 10);
         }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        
     }
 }
