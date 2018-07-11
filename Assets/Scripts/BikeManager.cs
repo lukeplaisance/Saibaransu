8a389101f6d61996137b0b0531e3e6e9c78967fa
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-07-11 12:11:54 -0500

    started working on game loop manager

---
+++ Assets/Scripts/BikeManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class BikeManager
+{
+    public Transform m_SpawnPoint;
+    [HideInInspector]
+    public GameObject m_Instance;
+    [HideInInspector]
+    public int m_RoundWin;
+    private BikeMovementBehaviour m_Movement;
+    private CameraControllerBehaviour m_Camera;
+
+    public void Setup()
+    {
+        m_Movement = m_Instance.GetComponent<BikeMovementBehaviour>();
+        m_Camera = m_Instance.GetComponent<CameraControllerBehaviour>();
+    }
+
+    public void DisableControls()
+    {
+        m_Movement.enabled = false;
+        m_Camera.enabled = false;
+    }
+
+    public void EnableControls()
+    {
+        m_Movement.enabled = true;
+        m_Camera.enabled = true;
+    }
+
+    public void Reset()
+    {
+        m_Instance.transform.position = m_SpawnPoint.position;
+        m_Instance.SetActive(true);
+        m_Instance.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public float m_NumRounds = 3f;
+    public float m_StartDelay = 3f;
+    public float m_EndDelay = 3f;
+    public CameraControllerBehaviour m_CameraOne;
+    public CameraControllerBehaviour m_CameraTwo;
+    public GameObject m_BikeOnePrefab;
+    public GameObject m_BikeTwoPrefab;
+    private GameObject m_SpawnedBikeOne;
+    private GameObject m_SpawnedBikeTwo;
+    public BikeManager m_BikeOne;
+    public BikeManager m_BikeTwo;
+    public Text m_Text;
+    public bool m_WasHit = false;
+
+    private int m_RoundNumber;
+    private WaitForSeconds m_StartWait;
+    private WaitForSeconds m_EndWait;
+    private BikeManager m_RoundWinner;
+    private BikeManager m_GameWinner;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_StartWait = new WaitForSeconds(m_StartDelay);
+        m_EndWait = new WaitForSeconds(m_EndDelay);
+
+        SpawnAllBikes();
+	}
+
+    private void SpawnAllBikes()
+    {
+        m_SpawnedBikeOne = Instantiate(m_BikeOnePrefab, m_BikeOne.m_SpawnPoint.position, Quaternion.identity) as GameObject;
+        m_SpawnedBikeTwo = Instantiate(m_BikeTwoPrefab, m_BikeTwo.m_SpawnPoint.position, Quaternion.Euler(0, 180, 0)) as GameObject;
+            
+        m_BikeOne.Setup();
+        m_BikeTwo.Setup();
+            
+    }
+
+    private IEnumerator GameLoop()
+    {
+        yield return StartCoroutine(RoundStart());
+        yield return StartCoroutine(RoundPlaying());
+        yield return StartCoroutine(RoundEnd());
+
+        if(m_GameWinner != null)
+        {
+            SceneManager.LoadScene("99.Luke");
+        }
+        else
+        {
+            StartCoroutine(GameLoop());
+        }
+    }
+
+    private IEnumerator RoundStart()
+    {
+        ResetAllBikes();
+        DisableBikeControl();
+
+        //m_cameraController.SetStartPosition();
+        m_RoundNumber++;
+
+        yield return m_StartWait;
+    }
+
+    private IEnumerator RoundPlaying()
+    {
+        EnableBikeControl();
+        m_Text.text = string.Empty;
+
+        while(m_WasHit == false)
+        {
+            yield return null;
+        }
+    }
+
+    private IEnumerator RoundEnd()
+    {
+        DisableBikeControl();
+        m_RoundWinner = null;
+        m_RoundWinner = GetRoundWinner();
+
+        if(m_RoundWinner != null)
+        {
+            m_RoundWinner.m_RoundWin++;
+        }
+
+        yield return m_EndWait;
+    }
+
+    private BikeManager GetRoundWinner()
+    {
+            if (Input.GetButton("Fire1"))
+            {
+                ResetAllBikes();
+            }
+        return null;
+    }
+
+    //private BikeManager GetGameWinner()
+    //{
+    //}
+
+    private void ResetAllBikes()
+    {
+        m_BikeOne.Reset();
+        m_BikeTwo.Reset();
+    }
+
+    private void EnableBikeControl()
+    {
+        m_BikeOne.EnableControls();
+        m_BikeTwo.EnableControls();
+    }
+
+    private void DisableBikeControl()
+    {
+        m_BikeOne.DisableControls();
+        m_BikeTwo.DisableControls();
+    }
+}
