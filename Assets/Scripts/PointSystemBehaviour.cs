a9d18559d2240c8cf0878e61c4476bac1c028314
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2018-07-23 15:12:59 -0500

    working on incrementing rounds

--- Assets/Scripts/PointSystemBehaviour.cs
+++ Assets/Scripts/PointSystemBehaviour.cs
@@ -1,14 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
 
 public class PointSystemBehaviour : MonoBehaviour
 {
     [SerializeField]
     private List<int> pointsList = new List<int>();
+    [SerializeField]
+    //private GameEvent gameEvent;
+    public Text scoreTotalText;
+    public Text currentRoundText;
+    private int totalRounds = 3;
+    private int currentRound = 0;
+    private int ScoreToWin = 50;
+    private int totalScore;
 
     public void AddPoints(int point)
     {
         pointsList.Add(point);
+        SetTotalScore();
     }
+
+    public void SetCurrentRound()
+    {
+        IncrementRound();
+        currentRoundText.text = "Round: " + currentRound.ToString();
+    }
+
+    public void SetTotalScore()
+    {
+        totalScore = pointsList.Sum();
+        scoreTotalText.text = "Total Score: " + totalScore.ToString();
+    }
+
+    public void IncrementRound()
+    {
+        for (int i = 0; i < totalRounds; i++)
+        {
+            currentRound++;
+            if(currentRound >= totalRounds)
+            {
+                currentRound = 0;
+            }
+        }
+    }
+
+    //public void CheckForWinner()
+    //{
+    //    for(int i = 0; i < ScoreToWin; i++)
+    //    {
+    //        if(totalScore == ScoreToWin)
+    //        {
+                
+    //        }
+    //    }
+    //}
 }
