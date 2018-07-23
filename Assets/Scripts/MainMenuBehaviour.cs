c1e5399a42df5c18bcc8da8242b677bd2a58da25
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-07-23 14:09:27 -0500

    Main Menu

---
+++ Assets/Scripts/MainMenuBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+    public string GamePlayScene;
+    public GameObject MainMenuObject;
+    public GameObject CreditsObject;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(GamePlayScene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ToggleCredits()
+    {
+        MainMenuObject.SetActive(!MainMenuObject.activeSelf);
+        CreditsObject.SetActive(!MainMenuObject.activeSelf);
+    }
+}
