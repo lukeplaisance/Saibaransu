fd41aa8e9fbf61faedd9b5dc3c96c26b5ed733d6
bstelly <bstelly1999@gmail.com>
2018-07-23 15:13:02 -0500

    Working on UI

---
+++ Assets/Scripts/UpdateUIBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUIBehaviour : MonoBehaviour
+{
+    public List<int> RoundScores;
+
+    public Text RoundScoreText;
+
+    public Text RoundNumberText;
+
+    public Text TotalScoreText;
+
+	void Start () {
+		
+	}
+
+    public void UpdateRoundNumberText()
+    {
+
+    }
+
+    public void updateRoundScoreText()
+    {
+
+    }
+
+    public void UpdateTotalScoreText()
+    {
+        int total = 0;
+        foreach (var score in RoundScores)
+        {
+            total += score;
+        }
+
+        TotalScoreText.text = "Total\n    Score: " + total;
+    }
+}
