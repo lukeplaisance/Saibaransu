5e8df38573331de642dc668434c58c832e61c804
bstelly <bstelly1999@gmail.com>
2018-07-23 16:33:23 -0500

    Gave PointSystemBehaviour a variable to hold current round's score
    To be used for tracking each round's score

--- Assets/Scripts/PointSystemBehaviour.cs
+++ Assets/Scripts/PointSystemBehaviour.cs
@@ -7,8 +7,11 @@ public class PointSystemBehaviour : MonoBehaviour
     [SerializeField]
     private List<int> pointsList = new List<int>();
 
+    public int currentRoundScore;
+
     public void AddPoints(int point)
     {
         pointsList.Add(point);
+        currentRoundScore = point;
     }
 }
