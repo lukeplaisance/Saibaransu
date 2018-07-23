be5a7eb0dd29c8996a410f6085c25d14c54e7960
bstelly <bstelly1999@gmail.com>
2018-07-23 16:33:58 -0500

    Script for updating UI elements

--- Assets/Scripts/UpdateUIBehaviour.cs
+++ Assets/Scripts/UpdateUIBehaviour.cs
@@ -7,23 +7,56 @@ public class UpdateUIBehaviour : MonoBehaviour
 {
     public List<int> RoundScores;
 
+    public int roundNum;
+
     public Text RoundScoreText;
 
     public Text RoundNumberText;
 
     public Text TotalScoreText;
 
-	void Start () {
-		
+    public GameObject pointSystem;
+
+    private PointSystemBehaviour playerScore;
+
+
+
+
+    void Start ()
+	{
+	    playerScore = pointSystem.GetComponent<PointSystemBehaviour>();
+	    roundNum = 1;
 	}
 
     public void UpdateRoundNumberText()
     {
+        if (roundNum < 3)
+        {
+            roundNum += 1;
+        }
 
+        RoundNumberText.text = "Round: " + roundNum;
     }
+
+    int round2Score = 0;
 
     public void updateRoundScoreText()
     {
+        Debug.Log("update score event");
+        RoundScores.Add(playerScore.currentRoundScore);
+        RoundScoreText.text = "Scores\n" + "R1: " + playerScore.currentRoundScore + "\t";
+        if (roundNum == 2)
+        {
+            RoundScoreText.text += "R2: " + playerScore.currentRoundScore + "\t";
+            Debug.Log("Current score" + playerScore.currentRoundScore);
+            round2Score = playerScore.currentRoundScore;
+        }
+
+        if (roundNum == 3)
+        {
+            RoundScoreText.text += "R2: " + round2Score + "\t";
+            RoundScoreText.text += "R3: " + playerScore.currentRoundScore;
+        }
 
     }
 
