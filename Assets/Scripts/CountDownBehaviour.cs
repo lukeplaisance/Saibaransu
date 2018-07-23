d2ac822322bf7e2d09528d69687f57a32888c8ba
Dylan Guidry <guidry.dylan.95@gmail.com>
2018-07-23 15:08:01 -0500

    count fown

---
+++ Assets/Scripts/CountDownBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownBehaviour : MonoBehaviour
+{
+    public float StartTime = 3;
+    public float timer = 3;
+    public GameObject Bike;
+    public Text CountDown;
+
+    public void Start()
+    {
+        timer = StartTime;
+    }
+
+    public void ResetTimer()
+    {
+        timer = StartTime;
+        Bike.GetComponent<BikeMovementBehaviour>().enabled = false;
+        CountDown.enabled = true;
+    }
+
+    private void Update()
+    {
+        timer -= Time.unscaledDeltaTime;
+        int time = (int)timer + 1;
+        CountDown.text = time.ToString();
+        if(timer < 0)
+        {
+            Bike.GetComponent<BikeMovementBehaviour>().enabled = true;
+            CountDown.enabled = false;
+        }
+    }
+}
